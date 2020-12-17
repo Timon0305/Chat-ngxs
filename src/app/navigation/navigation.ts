85261f9d0bb73e7f88dc8ba44f1be0cd08eebd90
Timon0305 <timon0305@outlook.com>
2020-12-17 22:46:44 +0800

    first commit

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,26 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id       : 'applications',
+        title    : 'Applications',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        icon     : 'apps',
+        children : [
+            {
+                id       : 'chat',
+                title    : 'Chat',
+                translate: 'NAV.CHAT',
+                type     : 'item',
+                icon     : 'chat',
+                url      : '/apps/chat',
+                badge    : {
+                    title: '13',
+                    bg   : '#09d261',
+                    fg   : '#FFFFFF'
+                }
+            },
+        ]
+    }
+];
