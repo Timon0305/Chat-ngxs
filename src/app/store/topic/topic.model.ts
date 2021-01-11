c97bcee5016f94258d72cb005508a069e0825d9a
Timon0305 <timon0305@outlook.com>
2021-01-11 08:18:42 +0800

    fixed 6.3

--- src/app/store/topic/topic.model.ts
+++ src/app/store/topic/topic.model.ts
@@ -1,18 +1,18 @@
 export class TopicModel {
     id: string;
     data?: {
-        channelId: string;
-        name: string;
-        status: string;
-        stats: {
-            newMessages: number;
-        };
-        system: {
-            createdAt: string;
-            updatedAt: string;
-            spaceId: string;
-            userId: string;
-            model: number;
+        channelId?: string;
+        name?: string;
+        status?: string;
+        stats?: {
+            newMessages?: number;
         }
+    };
+    system?: {
+        createdAt?: string;
+        updatedAt?: string;
+        spaceId?: string;
+        userId?: string;
+        model?: number;
     }
 }
